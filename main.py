b6fe5faa2301914ed88c1419ac6907070d387e4d
Ferronn <ferronn@ferronn.dev>
2021-12-27 16:51:43 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from google.cloud import storage
+from google.cloud import tasks_v2
+import asyncio
+import json
+
+
+async def do_publish():
+    parent = (
+        "projects/www-wowless-dev/locations/us-central1/queues/addon-downloads"
+    )
+    tasks_client = tasks_v2.CloudTasksAsyncClient()
+    async for _ in await tasks_client.list_tasks(parent=parent):
+        print("tasks are present, so nothing to do")
+        return
+    print("creating tasks...")
+    await asyncio.gather(
+        *[
+            tasks_client.create_task(
+                parent=parent,
+                task={
+                    "http_request": {"url": f'http://localhost/moo{x["id"]}'}
+                },
+            )
+            for x in json.loads(
+                storage.Client()
+                .bucket("wowless.dev")
+                .blob("addons.json")
+                .download_as_bytes()
+            )["cf"]
+        ]
+    )
+    print("created tasks")
+
+
+def publish(_):
+    asyncio.run(do_publish())
+    return ""
+
+
+if __name__ == "__main__":
+    publish(None)
